fa8316b0b212545440e8fd994bc55b38fa922cf9
ocaiohen <caiohencontato@gmail.com>
2024-04-08 19:38:40 -0300

    Delete quase feito, falta consertar bug.

--- src/Pages/Orders Page/App.js
+++ src/Pages/Orders Page/App.js
@@ -1,26 +1,61 @@
 import { useNavigate } from "react-router-dom";
 import React, {useState, useEffect} from "react"
+import axios from "axios"
 import 
 {
     MainContainer,
     MainContent,
     H1,
-    GoBackButton
+    GoBackButton,
+    ListOfOrders,
+    OrderCard,
+    OrderTextContainer,
+    DeleteButton,
+    OrderText,
+    NameText
 } 
 from "./Styles.js";
 import orderImage from "../../Assets/orderImage.png"
 function App() {
+    const [orders, setOrders] = useState([])
     const navigate = useNavigate()
+
+    useEffect(()=>{
+        const getAndSetOrders = async () => {
+          const {data: newOrders} = await axios.get("http://localhost:3001/documents")
+          setOrders(newOrders)
+        }
+        getAndSetOrders()
+    },[])
+
+    const deleteUser = async (id) =>{
+        axios.delete(`http://localhost:3001/documents/${id}`)
+        const {data: newOrders} = await axios.get("http://localhost:3001/documents")
+        setOrders(newOrders)
+    }
 
     const goBack = () =>{
         navigate("/")
     }
     return (
         <MainContainer>
-            <img height = "300px" src = {orderImage}></img>
+            <img height = "200px" alt = "imagem-pedidos" src = {orderImage}></img>
             <MainContent>
                 <H1>Pedidos</H1>
-
+                <ListOfOrders>
+                    {
+                        orders.map((order) => (
+                            <OrderCard key = {order.id}>
+                                <OrderTextContainer>
+                                    <OrderText>{order.order}</OrderText>
+                                    <NameText>{order.name}</NameText>
+                                </OrderTextContainer>
+                                
+                                <DeleteButton onClick={() => deleteUser(order.id)}></DeleteButton>
+                            </OrderCard>
+                        ))
+                    }
+                </ListOfOrders>
                 <GoBackButton onClick = {goBack}>Voltar</GoBackButton>
             </MainContent>
         </MainContainer>
--- src/Pages/Orders Page/Styles.js
+++ src/Pages/Orders Page/Styles.js
@@ -1,8 +1,9 @@
 import styled from "styled-components"
+import DeleteImage from "../../Assets/trash can icon.png"
 
 export const MainContainer = styled.main`
     min-height: 100vh;
-    width: 100vw;
+    max-width: 100vw;
     display: flex;
     flex-direction: column;
     align-items: center;
@@ -29,6 +30,55 @@ export const GoBackButton = styled.button`
     border: none;
     cursor: pointer;
     font-size: 16px;
+    &:hover{
+        opacity: 0.9;
+    }
+    &:active{
+        opacity: 0.7;
+    }
+`
+export const ListOfOrders = styled.ul`
+    width: 100%;
+    height: fit-content;
+    margin-top: 40px;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    gap: 20px;
+`
+export const OrderCard = styled.div`
+    width: 100%;
+    height: 80px;
+    padding: 30px;
+    background-color: rgba(255,255,255, 0.25);
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+    justify-content: center;
+    border-radius: 15px;
+`
+export const OrderTextContainer = styled.div`
+    width: 100%;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+`
+export const OrderText = styled.p`
+    font-size: 18px;
+    font-weight: 300;
+`
+export const NameText = styled.p`
+    font-size: 18px;
+    font-weight: 700;
+`
+export const DeleteButton = styled.button`
+    height: 32px;
+    width: 32px;
+    background-image: url("${DeleteImage}");
+    background-size: cover;
+    background-color: transparent;
+    border: none;
+    cursor: pointer;
     &:hover{
         opacity: 0.9;
     }
